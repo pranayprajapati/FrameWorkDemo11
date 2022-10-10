98279cbafbf2ede87c3782ca483be1a26718562e
pranay.prajapati <pranay.prajapati@auxanoglobalservices.com>
2022-10-10 19:02:11 +0530

    Initial Commit

---
+++ FrameWorkDemo11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FrameWorkDemo11
+//
+//  Created by MAcBook on 10/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
