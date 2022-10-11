438acef534dc2d6ae06c424ccee1bb2c045a9b95
pranay.prajapati <pranay.prajapati@auxanoglobalservices.com>
2022-10-11 10:53:21 +0530

    111

---
+++ FrameWork11/FrameWork11/FrameWork11.h
@@ -0,0 +1,18 @@
+//
+//  FrameWork11.h
+//  FrameWork11
+//
+//  Created by MAcBook on 10/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameWork11.
+FOUNDATION_EXPORT double FrameWork11VersionNumber;
+
+//! Project version string for FrameWork11.
+FOUNDATION_EXPORT const unsigned char FrameWork11VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameWork11/PublicHeader.h>
+
+
--- FrameWorkDemo11/ViewController.swift
+++ FrameWorkDemo11/ViewController.swift
@@ -6,14 +6,24 @@
 //
 
 import UIKit
+import FrameWork11
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .blue
+        
+        //self.navigationController?.pushViewController(FirstViewController(), animated: true)
+        //self.navigationController?.present(FirstViewController(), animated: true)
     }
 
-
+    @IBAction func buttonClick(_ sender: UIButton) {
+        self.navigationController?.pushViewController(FirstViewController(), animated: true)
+   
+    }
+    
 }
 
